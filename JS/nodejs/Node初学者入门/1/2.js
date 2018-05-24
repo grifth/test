706084effc184c76066fc347b7d71815b578238f
grifth <2473713213@qq.com>
2018-05-24 14:57:17 +0800

    node

---
+++ JS/nodejs/Node初学者入门/1/2.js
@@ -0,0 +1,9 @@
+var http = require("http");
+function onRequest(request, response){
+  console.log("Request received.");
+response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});   
+response.write("Hello World");
+  response.end();
+}
+http.createServer(onRequest).listen(8888);
+console.log("Server has started.");
---
+++ JS/nodejs/Node初学者入门/2/server.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var url = require("url");
+function start(route, handle){
+  function onRequest(request, response){
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for "+ pathname +" received.");
+    route(handle, pathname);
+response.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});   
+    response.write("Hello World");
+    response.end();
+  }
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+exports.start = start;
+
