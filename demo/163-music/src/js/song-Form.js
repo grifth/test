fec464679265c835e8cae9f29264cdfa8393b7e0
grifth <2473713213@qq.com>
2018-06-10 23:57:34 +0800

    存档

--- demo/163-music/src/js/song-Form.js
+++ demo/163-music/src/js/song-Form.js
@@ -1,138 +1,201 @@
+// {
+//     let view = {
+//         el: '.page>main',
+//         init() {
+//             this.$el = $(this.el)
+//         },
+//         template: `            
+//     <h1>新建歌曲</h1>
+//             <form class="form">
+//                 <div class="row">
+//                     <label>
+//                       歌名
+//                     </label>
+//                     <input name="name" type="text" value="__name__">
+//                 </div>
+//                 <div class="row">
+//                     <label>
+//                       歌手
+//                     </label>
+//                     <input name="singer" type="text" value="__singer__">
+//                 </div>
+//                 <div class="row">
+//                     <label>
+//                       外链
+//                     </label>
+//                     <input name="url" type="text" value="__url__">
+//                 </div>
+//                 <div class="row">
+//                     <button type="submit">保存</button>
+//                 </div>                            
+//             </form>
+//             `,
+//         //data={} 如果梅有传形参数 或者形参为undefined
+//         render(data = {}) {
+//             console.log(1)
+//             let placeholders = ['name', 'url', 'id', 'singer']
+//             let html = this.template
+//             placeholders.map((string) => {
+//                 html = html.replace(`__${string}__`, data[string] || '')
+//             })
+//             $(this.el).html(html)
+//         }
+//     }
+//     let model = {
+//         data: {
+//             name: '',
+//             singer: '',
+//             url: '',
+//             id: ''
+//         },
+//         create(data) {
+//             // 声明类型
+//             var Song = AV.Object.extend('Song');
+//             // 新建对象
+//             var song = new Song();
+//             // 设置名称
+//             song.set('name', data.name);
+//             song.set('singer', data.singer);
+//             song.set('url', data.url);
+//             // 设置优先级
+//             return song.save().then((newSong) => {
+//                 let { id, attributes } = newSong
+//                 this.data = { id, ...attributes }
+//             }, (error) => {
+//                 console.error(error);
+//             });
+//         }
+//     }
+//     let controller = {
+//         init(view, model) {
+//             this.view = view,
+//                 this.model = model,
+//                 this.view.init(),
+//                 this.view.render(),
+//                 this.bindEvents(),
+//                 window.eventHub.on('upload', (data) => {
+//                     this.view.render(data)
+//                 }),
+//                 window.eventHub.on('select', (data) => {
+//                     console.log(data)
+//                 })
+//         },
+//         active() {
+//             $(this.view.el).addClass('active')
+//         },
+//         bindEvents() {
+//             this.view.$el.on('submit', 'form', (e) => {
+//                 e.preventDefault()
+//                 let needs = 'name singer url'.split(' ')
+//                 let data = {}
+//                 needs.map((string) => {
+//                     data[string] = this.view.$el.find(`input[name="${string}"]`).val()
+//                 })
+//                 this.model.create(data)
+//                     .then(() => {
+//                         this.view.render({})
+//                         let obj = JSON.parse(JSON.stringify(this.model.data))
+//                         window.eventHub.emit('creat', obj)
+//                     })
+//             })
+//         }
+
+
+//     }
+//     controller.init(view, model)
+// }
 {
-	let view = {
-		el:'.page>main',
-        init(){
-            this.$el = $(this.el)
-        },
-		template:`            
-		<h1>新建歌曲</h1>
-            <form class="form">
-                <div class="row">
-                    <label>
-                      歌名
-                    </label>
-                    <input name="name" type="text" value="__name__">
-                </div>
-                <div class="row">
-                    <label>
-                      歌手
-                    </label>
-                    <input name="singer" type="text" value="__singer__">
-                </div>
-                <div class="row">
-                    <label>
-                      外链
-                    </label>
-                    <input name="url" type="text" value="__url__">
-                </div>
-                <div class="row">
-                    <button type="submit">保存</button>
-                </div>                            
-            </form>
-            `,
-            //data={} 如果梅有传形参数 或者形参为undefined
-            render( data={}){
-<<<<<<< HEAD
-                let placeholders = ['name','url','id','singer']
-=======
-<<<<<<< HEAD
-                let placeholders = ['name','url']
-=======
-                let placeholders = ['name','url','singer','id']
->>>>>>> 2aa3374d1a1468a4dc91377a0c877e22e59daecf
->>>>>>> f71bb9b5120e0a9931b5959c4ef2350e62441633
-                let html = this.template
-                placeholders.map((string)=>{
-                    html=html.replace(`__${string}__`,data[string]||'')
-                })
-            	$(this.el).html(html)
-            },
-            reset(){
-                this.render({})
-            }
-	}
-	let model = {
-        data:{
-            name:'',singer:'',url:'',id:
-''    },
-        create(data){
-             // 声明类型
-              var Song = AV.Object.extend('Song');
-              // 新建对象
-              var song = new Song();
-              // 设置名称
-              song.set('name',data.name);
-              song.set('singer',data.singer);
-              song.set('url',data.url);
-              // 设置优先级
-<<<<<<< HEAD
-              return song.save().then( (newSong)=>{
-                let { id , attributes} = newSong
-                Object.assign(this.data,{id,...attributes})
-=======
-             return song.save().then( (newSong)=>{
-                let { id , attributes} = newSong
-<<<<<<< HEAD
-               this.data = {id,...attributes}
-                // console.log(this.data);
-=======
-                Object.assign(this.data,{id:id,...attributes,})
->>>>>>> 2aa3374d1a1468a4dc91377a0c877e22e59daecf
->>>>>>> f71bb9b5120e0a9931b5959c4ef2350e62441633
-              },  (error)=>{
-                console.error(error);
-              });
-        }
+  let view = {
+    el: '.page > main',
+    init(){
+      this.$el = $(this.el)
+    },
+    template: `
+      <h1>新建歌曲</h1>
+      <form class="form">
+        <div class="row">
+          <label>
+          歌名
+          </label>
+          <input name="name" type="text" value="__name__">
+        </div>
+        <div class="row">
+          <label>
+          歌手
+          </label>
+          <input name="singer" type="text" value="__singer__">
+        </div>
+        <div class="row">
+          <label>
+          外链
+          </label>
+          <input name="url" type="text" value="__url__">
+        </div>
+        <div class="row actions">
+          <button type="submit">保存</button>
+        </div>
+      </form>
+    `,
+    render(data = {}){
+      let placeholders = ['name', 'url', 'singer', 'id']
+      let html = this.template
+      placeholders.map((string)=>{
+        html = html.replace(`__${string}__`, data[string] || '')
+      })
+      $(this.el).html(html)
+    },
+    reset(){
+      this.render({})
     }
-	let controller = {
-		init(view,model){
-			this.view = view,
-            this.view.init(),
-			this.model = model,
-			this.view.render(),
-            this.bindEvents(),
-            window.eventHub.on('upload',(data)=>{
-                this.model.data = data
-                this.view.render(this.model.data)
-            }),
-            window.eventHub.on('select',(data)=>{
-                this.model.data = data
-                this.view.render(this.model.data)
-            })
-		},
-        active(){
-          $(this.view.el).addClass('active')
-        },
-        bindEvents(){
-            this.view.$el.on('submit','form',(e)=>{
-                e.preventDefault()
-                let needs = 'name singer url'.split(' ')
-                let data ={}
-                needs.map((string)=>{
-                    data[string]=this.view.$el.find(`input[name="${string}"]`).val()
-                })
-                this.model.create(data)
-                    .then(()=>{
-<<<<<<< HEAD
-                      this.view.render({})
-                      let obj =  JSON.parse(JSON.stringify(this.model.data))
-                      window.eventHub.emit('creat',obj)
-=======
-<<<<<<< HEAD
-                        // console.log(this.model.data);
-                      // this.view.render(this.model.data)  
-                      this.view.reset()
-                      let string = JSON.stringify(this.model.data)
-                      let object = JSON.parse(string)
-                      window.eventHub.emit('create',object)
-=======
-                      this.view.reset(); 
-                      window.eventHub.emit('create',this.model.data)
->>>>>>> 2aa3374d1a1468a4dc91377a0c877e22e59daecf
->>>>>>> f71bb9b5120e0a9931b5959c4ef2350e62441633
-                    })
-            })
-        }
-	}
-	controller.init(view,model)
+  }
+  let model = {
+    data: {
+      name: '', singer: '', url: '', id: ''
+    },
+    create(data){
+      var Song = AV.Object.extend('Song');
+      var song = new Song();
+      song.set('name',data.name);
+      song.set('singer',data.singer);
+      song.set('url',data.url);
+      return song.save().then((newSong) =>{
+        let {id, attributes} = newSong
+        Object.assign(this.data, { id, ...attributes })
+      }, (error) =>{
+        console.error(error);
+      });    
+    }
+  }
+  let controller = {
+    init(view, model){
+      this.view = view
+      this.view.init()
+      this.model = model
+      this.view.render(this.model.data)
+      this.bindEvents()
+      window.eventHub.on('upload', (data)=>{
+        this.model.data = data
+        this.view.render(this.model.data)
+      })
+    },
+    bindEvents(){
+      this.view.$el.on('submit', 'form', (e)=>{
+        e.preventDefault()
+        let needs = 'name singer url'.split(' ')
+        let data = {}
+        needs.map((string)=>{
+          data[string] = this.view.$el.find(`[name="${string}"]`).val()
+        })
+        this.model.create(data)
+          .then(()=>{
+            this.view.reset()
+            //this.model.data === 'ADDR 108'
+            let string = JSON.stringify(this.model.data)
+            let object = JSON.parse(string)
+            window.eventHub.emit('create', object)
+          })
+      })
+    }
+  }
+  controller.init(view, model)
 }
--- demo/163-music/src/js/song-list.js
+++ demo/163-music/src/js/song-list.js
@@ -3,35 +3,14 @@
 		el:'#songList-container',
 		template:`
                 <ul class="songList" >
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 2aa3374d1a1468a4dc91377a0c877e22e59daecf
->>>>>>> f71bb9b5120e0a9931b5959c4ef2350e62441633
                 </ul> 
                 `,
         render(data){
             let $el = $(this.el)
-<<<<<<< HEAD
             $el.html(this.template)
             let {songs} = data
-            let liList = songs.map((song)=>$('<li></li>').text(song.name)
+            let liList = songs.map((song)=>$('<li></li>').text(song.name).attr('data-song-id',song.id)
             )
-=======
-            let {songs} = data
-            $el.html(this.template)
-            let liList =songs.map((song)=>$('<li></li>').text(song.name))
-            $el.find('ul').empty()
-            liList.map((domLi)=>{
-                $el.find('ul').append(domLi)
-            })
-        	$(this.el).html(this.template)
-            let {songs} = data
-            let $el = $(this.el)
-            let liList = songs.map((song)=>$('<li></li>').text(song.name).attr('data-song-id',song.id))
->>>>>>> f71bb9b5120e0a9931b5959c4ef2350e62441633
             liList.map((domLi)=>{
                 $el.find('ul').append(domLi)
             })
@@ -40,39 +19,22 @@
             $(this.el).find('.active').removeClass('active')
         },
         activeItem(li){
-                let $li = $(li)
-                $li.addClass('active')
-                            .siblings('.active').removeClass('active')
+            $(li).addClass('active')
+                        .siblings('.active').removeClass('active')
         }
-
 	}
-<<<<<<< HEAD
-	let model = {
-        data:{
-            songs:[
-            {id:1,name:'1'}]
-=======
-<<<<<<< HEAD
-
 	let model = {
         data:{
             songs:[]
         },
         find(){
-            var query = new AV.Query('Song');
+            var query = new AV.Query('Song')
             return query.find().then((songs)=>{
-                this.data.songs = songs.map((song)=>{
-                    return {id:song.id,...song.attributes}
+                 this.data.songs = songs.map((song)=>{
+                     return{id:song.id, ...song.attributes}
                 })
                 return songs
             })
-=======
-	let model = {
-        data:{songs:[
-            {id:1,name:'1'},{id:2,name:'2'}
-            ]
->>>>>>> 2aa3374d1a1468a4dc91377a0c877e22e59daecf
->>>>>>> f71bb9b5120e0a9931b5959c4ef2350e62441633
         }
     }
 	let controller ={
@@ -80,51 +42,39 @@
 			this.view = view,
 			this.model = model,
 			this.view.render(this.model.data)
-            this.bindEvents()
             this.getAllSongs()
+            this.bindEvents()
 		},
-        getAllSongs(){
-            this.model.find().then(()=>{
-                this.view.render(this.model.data)
-            })        
-        },
-        bindEvents(){
-            $(this.view.el).on('click','li',(e)=>{
-                this.view.activeItem(e.currentTarget)
-                let songId = e.currentTarget.getAttribute('data-song-id')
-                let data 
-                let songs = this.model.data.songs
-                for(let i = 0;i<songs.length;i++){
-                    if(songs[i].id===songId){
-                        data = songs[i]
+             getAllSongs(){
+                    this.model.find().then(()=>{
+                    this.view.render(this.model.data)
+                 })        
+            },
+             bindEvents(){
+                $(this.view.el).on('click','li',(e)=>{
+                    this.view.activeItem(e.currentTarget)
+                    let songId = e.currentTarget.getAttribute('data-song-id')
+                    let data 
+                    let songs = this.model.data.songs
+                    for(let i = 0;i<songs.length;i++){
+                        if(songs[i].id===songId){
+                            data = songs[i]
+                            break
+                        }
                     }
-                }
-                window.eventHub.emit('select',JSON.parse(JSON.stringify(data)))
-            })
-        },
-        bindEventHub(){
-            window.eventHub.on('upload',()=>{
-                this.view.clearActive()
-            })
-            window.eventHub.on('create',(songdata)=>{
-<<<<<<< HEAD
-                console.log(songdata)
-                this.model.data.songs.push(songdata)
-                this.view.render(this.model.data)
-=======
-<<<<<<< HEAD
-                this.model.data.songs.push(songdata)
-                this.view.render(this.model.data)
-            })            
-        }
-=======
-                console.log(songdata)
-                this.model.data.push(songdata)
-                this.view.render(this.model.data)
->>>>>>> f71bb9b5120e0a9931b5959c4ef2350e62441633
-            })
-		}
->>>>>>> 2aa3374d1a1468a4dc91377a0c877e22e59daecf
+                    window.eventHub.emit('select',JSON.parse(JSON.stringify(data)))
+                })
+            },
+             bindEventHub(){
+                    window.eventHub.on('upload',()=>{
+                    this.view.clearActive()
+                })
+                     window.eventHub.on('create',(songdata)=>{
+                        console.log(1)
+                    this.model.data.songs.push(songdata)
+                    this.view.render(this.model.data)
+                })
+             }
 	}
 	controller.init(view,model)
 }
