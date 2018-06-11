4c8fa1f5fd3862e0092320938b0438a822c15b5f
grifth <2473713213@qq.com>
2018-06-12 00:15:59 +0800

    lesson3.5

--- demo/163-music/src/js/song-list.js
+++ demo/163-music/src/js/song-list.js
@@ -8,12 +8,12 @@
     render(data){
       let $el = $(this.el)
       $el.html(this.template)
-      let {songs} = data
-      let liList = songs.map((song)=>{
-        let $li = $('<li></li>').text(song.name).attr('data-song-id', song.id)
-        if(song.id===selectSongId){
-          $li.addClass('active')
-        }
+      let {songs,selectSongId} = data
+      let liList = songs.map((song)=> {
+      let  $li = $('<li></li>').text(song.name).attr('data-song-id', song.id)
+      if(song.id===selectSongId){$li.addClass('active')}
+        return $li
+
       })
       $el.find('ul').empty()
       liList.map((domLi)=>{
@@ -63,6 +63,7 @@
         let songId = e.currentTarget.getAttribute('data-song-id')
         this.model.data.selectSongId = songId
         this.view.render(this.model.data)
+
         let data
         let songs = this.model.data.songs
         for(let i = 0;i<songs.length;i++){
